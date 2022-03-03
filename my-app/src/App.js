78bfadf378b775489bfe5b6fd2ff7fa0b447eaff
JJIMINSHIN <scalla1031@gmail.com>
2022-03-03 20:46:55 +0900

    update

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Template from './component/Template';
+import TodoList from './component/TodoList';
+
+const App = () =>{
+  return (
+    <Template>
+      <TodoList/>
+    </Template>
+  );
+}
+
+export default App;
