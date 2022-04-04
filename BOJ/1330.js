8fbe262b7eccf1b322ccf1041e97052e12b9dba0
JJIMINSHIN <scalla1031@gmail.com>
2022-04-04 22:25:53 +0900

    두 수 비교하기

---
+++ BOJ/1330.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const input = fs.readFileSync("/dev/stdin").toString.trim().split().map(Number);
+
+
+const [a,b] = [input[0], input[1]];
+
+if(a >b) {
+    console.log(">");
+}
+if(a < b) {
+    console.log("<");
+}
+if(a === b) {
+    console.log("==");
+}
+
+
