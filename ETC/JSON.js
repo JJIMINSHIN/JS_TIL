181b67f1575b9ec63d10388e4d249ea5278763df
JJIMINSHIN <scalla1031@gmail.com>
2022-04-07 21:05:36 +0900

    JSON

---
+++ ETC/JSON.js
@@ -0,0 +1,67 @@
+ //JSON
+//JavaScript Object Notatopm
+
+//1. Object to Json
+letjson=JSON.stringify(true);
+console.log(json);
+
+json=JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+constrabbit={
+    name : 'tori',
+    color :'white',
+    birhDate: newDate(),
+    jump: () =>{
+        console.log(`${name} can jump!`);
+    },
+};
+
+json=JSON.stringify(rabbit);
+console.log(json);
+
+//JSON으로 변환되는 것을 통제하고 싶다면 콜백함수 이용
+//해당 property만 변환
+json=JSON.stringify(rabbit, ["name","color"]);
+console.log(json);
+
+json=JSON.stringify(rabbit, (key, value) =>{
+   console.log(`key :${key}, value : ${value}`);
+   return key === 'name' ? 'ellie': value;
+});
+
+console.log(json);
+
+
+letstandardObj= {
+    'a': 'b',
+    'aObj' : {
+        aa: 'aa',
+    },
+    'array':[
+        {aa:'aa'}
+    ]
+};
+
+//a의 value를 가져와보자
+console.log(standardObj.a)
+
+//aObj value를 가져와보자
+console.log(standardObj.aObj)
+
+//array value를 가져와보자
+console.log(standardObj.array)
+
+//array에 bb:'bb'를 넣어보자
+standardObj.array.push({
+    aa:'zz',
+    bb:'ll'
+});
+
+
+//aa를 뽑아보자
+
+console.log('>>',standardObj.array);
+console.log('[]',standardObj['aObj']['aa'])
+console.log('===',standardObj['array'][0]['aa']);
+console.log('===',standardObj['array'][1]['aa']);
