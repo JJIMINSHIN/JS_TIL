7194148b9500fcf9af9443134924099b3fe449ad
JJIMINSHIN <scalla1031@gmail.com>
2022-04-17 15:36:07 +0900

    관계연산자

--- programmers/JS_Basic/relation_operator.js
+++ programmers/JS_Basic/relation_operator.js
@@ -7,3 +7,8 @@ function solution(a, b){
 
 
 //함수 solution의 내용을 수정해서 a와 b가 모두 True라면 True를, 그렇지 않으면 False를 return하도록 만들어 보세요.
+
+function solution(a,b){
+    //이 아래줄을 수정해서 a와 b가 모두 True인지를 return하도록 만들어 보세요.
+    return a && b
+}
