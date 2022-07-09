71e413f86493b5a8af2782a01155ed8e77ec1282
JJIMINSHIN <scalla1031@gmail.com>
2022-07-09 19:32:31 +0900

    최솟값 구하기

---
+++ JS_Algorithm/1_기본문제풀이/5.js
@@ -0,0 +1,10 @@
+function solution(arr){
+    let min = arr[0];
+    for(let i =0; i<arr.length; i++){
+        if(arr[i]<min) {
+            min = arr[i];
+        }
+    }
+    return min
+}
+console.log('sol',solution([5, 3, 7, 11, 2, 15, 17]))
