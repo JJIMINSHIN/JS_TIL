57e7719d132c8fdffc2af65f3b1cd34e3f9c31f5
JJIMINSHIN <scalla1031@gmail.com>
2022-07-17 15:29:06 +0900

    10부제

--- JS_Algorithm/1_기본문제풀이/7.js
+++ JS_Algorithm/1_기본문제풀이/7.js
@@ -6,5 +6,11 @@ function solution(day, arr){
     return answer;
 }
 
+function solution2(day, arr){
+    let cars = arr.filter((answer) => answer % 10 === day);
+    return cars.length
+}
+
 arr=[25, 23, 11, 47, 53, 17, 33];
 console.log(solution(3,arr))
+console.log(solution2(3,arr))
