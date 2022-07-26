9f0d9c6958c1e6f24138864d7e3d41659f0018a9
JJIMINSHIN <scalla1031@gmail.com>
2022-07-26 16:05:45 +0900

    문자열 세기 .length

---
+++ node_200/014.js
@@ -0,0 +1,7 @@
+const str = 'hello';
+const str2 ='the method then selects elemets from the start argument';
+
+console.log('str length : ', str.length);
+console.log('str2 length : ', str2.length);
+console.log('str[0] : ', str[0]);
+console.log('str2[54] : ', str2[54]);
