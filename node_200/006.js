cf67e7d1f5c5985dda4314c5b94e75c998fe76ac
JJIMINSHIN <scalla1031@gmail.com>
2022-07-26 16:01:56 +0900

    백틱을 이용한 포맷팅

---
+++ node_200/006.js
@@ -0,0 +1,11 @@
+//`${변수}` 백틱을 이용한 포맷팅
+
+const greeting1 = '안녕';
+const greeting2 = '잘가';
+const name1 = '지민';
+const name2 = '명은';
+
+console.log(`${greeting1} ${name2}아 `);
+console.log(`${name1}이가 ${name2}이에게 ${greeting2}라고 인사를 한다`)
+
+
