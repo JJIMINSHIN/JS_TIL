585f8586866d3ef4bf1ed440605a25e666c37888
JJIMINSHIN <scalla1031@gmail.com>
2022-07-26 16:11:51 +0900

    해당 문자열 찾기 indexOf

---
+++ node_200/015.js
@@ -0,0 +1,9 @@
+const str ='hello';
+const str1 ='helelelelelloooou';
+const str2 = 'everybody say hello';
+
+console.log('hel: ', str.indexOf('hel'));
+console.log('el: ', str1.indexOf('el'));
+console.log('bye : ', str2.indexOf('bye'));
+
+//값이 없으면 -1 반환
