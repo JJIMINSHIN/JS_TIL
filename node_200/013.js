300b087e1d695bb08811ca1f7822eab529b7e85d
JJIMINSHIN <scalla1031@gmail.com>
2022-07-26 16:02:23 +0900

    나머지 구하기- 홀,짝 판단

---
+++ node_200/013.js
@@ -0,0 +1,8 @@
+console.log('5 % 2 : ', 5%2);
+
+const nums = [5, 6, 7, 8];
+
+console.log(nums[0], '을 2로 나누면 나머지는 ', nums[0] % 2,'로 홀수입니다.');
+console.log(nums[1], '을 2로 나누면 나머지는 ', nums[1] % 2,'로 짝수입니다.');
+console.log(nums[2], '을 2로 나누면 나머지는 ', nums[2] % 2,'로 홀수입니다.');
+console.log(nums[3], '을 2로 나누면 나머지는 ', nums[3] % 2,'로 짝수입니다.');
