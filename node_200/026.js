1e57b7b4f267a61bc5ad59ea9480cbaee348c5c1
JJIMINSHIN <scalla1031@gmail.com>
2022-07-26 16:40:34 +0900

    삼항연산자

---
+++ node_200/026.js
@@ -0,0 +1,9 @@
+const num1 = 1;
+const num2 = 2;
+const list = [1, 2, 3, 4];
+
+
+const result = num1 >num2 ? 'num1이 더 큽니다.': 'num2가 더 큽니다.';
+console.log(result);
+
+list.length > 0 ? console.log(list) : console.log('list가 비었씁니다.')
