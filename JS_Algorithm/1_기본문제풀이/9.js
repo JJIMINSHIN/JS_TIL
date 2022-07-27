bcc2a16533c4d6349241ddcccdec2b6cd9adea1c
JJIMINSHIN <scalla1031@gmail.com>
2022-07-27 16:18:28 +0900

    9. 문자 찾기

--- JS_Algorithm/1_기본문제풀이/9.js
+++ JS_Algorithm/1_기본문제풀이/9.js
@@ -4,6 +4,15 @@ const Atoshap = (str) => {
         let newStr = str.replaceAll('A', '#');
         return newStr;
     }
-}
-
+};
 console.log( Atoshap('banana'));
+
+
+
+const Atoshap2 = (str) => {
+    str = str.toUpperCase();
+    str = str.split('').map((item) => item === 'A' ? '#' : item).join('')
+    return str;
+};
+
+console.log(Atoshap2('banana'));
