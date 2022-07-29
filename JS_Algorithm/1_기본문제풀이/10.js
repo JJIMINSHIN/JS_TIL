d957fa4a110cc4661d49d7a89248e261cf483d83
JJIMINSHIN <scalla1031@gmail.com>
2022-07-29 11:53:03 +0900

    10. 문자 찾기

--- JS_Algorithm/1_기본문제풀이/10.js
+++ JS_Algorithm/1_기본문제풀이/10.js
@@ -1,4 +1,10 @@
-const findStr = (n, m) => {
+const findStr =(n,m) =>{
+    let cnt =0
+    let result = n.split('').map((item) => item === m ? cnt++: '');
+    return cnt;
+}
+
+const findStr2 = (n, m) => {
     let cnt = 0;
     for (let i of n) {
         if (i === m) cnt++
@@ -7,3 +13,4 @@ const findStr = (n, m) => {
 }
 
 console.log(findStr('COMPUTERPROGRAMMING', 'R'));
+console.log(findStr2('COMPUTERPROGRAMMING', 'R'));
