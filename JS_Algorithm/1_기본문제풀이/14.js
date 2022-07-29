5e57455e1d8ce017c548c9cb5d01ac35923101e7
JJIMINSHIN <scalla1031@gmail.com>
2022-07-29 11:53:56 +0900

    14. 가장 긴 문자열

---
+++ JS_Algorithm/1_기본문제풀이/14.js
@@ -0,0 +1,18 @@
+const input =(n, str) =>{
+   let result = '';
+   for(let i=0; i<n; i++){
+    if(str[i].length > result.length) result = str[i]
+   }
+   return result;
+}
+
+const input2 =(n, str) =>{
+    let wordLength = str.map((item) => item.length);
+    let maxLength = Math.max(...wordLength);
+    let result = wordLength.indexOf(maxLength);
+    return str[result]
+
+}
+
+console.log(input(5, ['teacher','time','student','beautiful','good']));
+console.log(input2(5, ['teacher','time','student','beautiful','good']))
