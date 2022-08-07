c13b0a8d2893f0d9e059d3b5dedda04db53537e5
JJIMINSHIN <scalla1031@gmail.com>
2022-08-07 15:36:46 +0900

    2. 보이는 학생

--- JS_Algorithm/2_1,2차원배열탐색/1.js
+++ JS_Algorithm/2_1,2차원배열탐색/1.js
@@ -1,1 +1,11 @@
-test
+const test = ( arr) =>{
+    let newOut =[];
+    arr.reduce((acc, cur) =>{
+        if(cur > acc) newOut.push(cur);
+        return cur;
+    },0)
+
+    return newOut;
+}
+
+console.log(test([7, 3, 9, 5, 6, 12]))
--- JS_Algorithm/2_1,2차원배열탐색/2.js
+++ JS_Algorithm/2_1,2차원배열탐색/2.js
@@ -9,4 +9,13 @@ const solution = (arr) =>{
     return answer
 }
 
+
+const sol = (arr) =>{
+    return arr.sort((a,b) => a-b).filter((item) =>{
+        let idx = arr.indexOf(item);
+        if(arr[idx] < arr[idx+1]) return item
+    }).length
+}
+
 console.log(solution([130, 135, 148, 140, 145, 150, 150, 153]))
+console.log(sol([130, 135, 148, 140, 145, 150, 150, 153]))
