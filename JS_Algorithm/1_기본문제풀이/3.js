be50df348775ce7b6ee4c908239b880f5e242027
JJIMINSHIN <scalla1031@gmail.com>
2022-10-01 15:42:20 +0900

    update

--- JS_Algorithm/1_기본문제풀이/3.js
+++
@@ -1,8 +0,0 @@
-function pensil(n){
-    let answer;
-    let pen = 12;
-    answer = Math.ceil(n/pen);
-    return answer;
-}
-
-console.log(pensil(178));
--- JS_Algorithm/1_기본문제풀이/4.js
+++
@@ -1,9 +0,0 @@
-function num(n){
-    let sum=0;
-    for(let i =1; i<=n; i++){
-        sum += i;
-    }
-    return sum;
-}
-
-console.log(num(6));
