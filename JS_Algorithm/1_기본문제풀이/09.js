4a472e643b7892e52e64c94a3e88001863052e93
JJIMINSHIN <scalla1031@gmail.com>
2022-10-01 15:41:53 +0900

    9. A를 #으로

---
+++ JS_Algorithm/1_기본문제풀이/09.js
@@ -0,0 +1,17 @@
+const Atoshap = (str) => {
+    str = str.toUpperCase();
+    if (str.indexOf('A')) {
+        let newStr = str.replaceAll('A', '#');
+        return newStr;
+    }
+};
+console.log(Atoshap('banana'));
+
+
+
+const Atoshap2 = (str) => {
+    str = str.toUpperCase();
+    str = str.split('').map((item) => item === 'A' ? '#' : item).join('')
+    return str;
+};
+console.log(Atoshap2('banana'));
