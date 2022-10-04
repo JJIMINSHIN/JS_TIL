4c43b17a5d662b3e5c2b0352214400ff3af14020
JJIMINSHIN <scalla1031@gmail.com>
2022-10-04 20:56:21 +0900

    정처기 문풀

--- JS_Tips/test.js
+++ JS_Tips/test.js
@@ -1,5 +1,14 @@
-let i = 0;
-do{
-    console.log(i);
-    i++
-}while(i<3); //0 1 2
+let a=sum =0;
+
+while(a<10){
+    a++;
+    if(a%2 ==1) {
+        console.log('a의 홀수 값 : ', a)
+        continue;s
+    }
+    console.log('증가된  a값: ', a)
+    sum +=a;
+    console.log('합계 : ', sum)
+
+ 
+}
