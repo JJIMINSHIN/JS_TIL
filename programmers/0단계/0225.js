fa5edff5e2bd977c7601b24c16f9cf21474b495a
JJIMINSHIN <scalla1031@gmail.com>
2023-04-05 16:19:50 +0900

    REFACT 이름 변경

--- programmers/0단계/0225.js
+++
@@ -1,7 +0,0 @@
-// 중복된 문자 제거
-
-function solution(my_string) {
-    return my_string.split('').filter((x, idx) => my_string.indexOf(x) === idx).join('')
-}
-
-console.log(solution("We are the world"	));
--- programmers/0단계/0226.js
+++
@@ -1,7 +0,0 @@
-// 피자 나눠먹기(3)
-
-function solution(slice, n) {
-    return n % slice !== 0 ? Math.floor(n /slice)+1 : Math.floor(n /slice)
-}
-
-console.log(solution(7,10))
