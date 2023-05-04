a4e758f00aa8e9bd716c96c17496abc991edf283
unknown <scalla1031@gmail.com>
2023-05-04 21:05:16 +0900

    CREATE : 배열의 원소 삭제하기

---
+++ programmers/basic/배열의 원소 삭제하기.js
@@ -0,0 +1,9 @@
+// 배열의 원소 삭제하기
+
+function solution(arr, delete_list) {
+    return arr.filter((v) => !delete_list.includes(v));
+}
+
+let arr = [293, 1000, 395, 678, 94];
+let delete_list = 	[94, 777, 104, 1000, 1, 12];
+console.log(solution(arr, delete_list));
