8cb584c22554ee43e19d36ab8487966869d1ffcf
JJIMINSHIN <scalla1031@gmail.com>
2023-07-22 17:05:21 +0900

    CREATE: 2의 영역

--- programmers/0단계/2의 영역.js
+++ programmers/0단계/2의 영역.js
@@ -1,1 +1,26 @@
 // 2의 영역
+
+function solution(arr) {
+    var answer= [];
+    let idx = arr.indexOf(2);
+    let len = arr.lastIndexOf(2);
+    if(idx != -1){
+        answer = arr.slice(idx,len+1);
+    }
+    else{
+        answer.push(-1);
+    }
+    
+    return answer;
+}
+
+// 더 간결하게 
+const solution2 = (arr) =>{
+    let idx = arr.indexOf(2);
+    let len = arr.lastIndexOf(2);
+    return !arr.includes(2) ? [-1] : arr.slice(idx,len+1);   
+}
+
+//let arr = [1, 1, 1]
+let arr =[1, 2, 1, 2, 1, 10, 2, 1];
+console.log(solution2(arr))
