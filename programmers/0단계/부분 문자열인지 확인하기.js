2a9978460e759b83cfaeee4329609b1438be4154
JJIMINSHIN <scalla1031@gmail.com>
2023-07-23 18:28:30 +0900

    CREATE : 부분 문자열인지 확인하기

--- programmers/0단계/부분 문자열인지 확인하기.js
+++ programmers/0단계/부분 문자열인지 확인하기.js
@@ -1,1 +1,19 @@
-// 부분 문자열인지 확인하기
+//부분 문자열인지 확인하기
+
+
+function solution(my_string, target) {
+    let answer = 0;
+    let len = target.length;
+    for(let i = 0; i<my_string.length; i++){
+        if(my_string.slice(i,len+i) == target) answer =1;
+    }  
+    return answer; 
+
+}
+
+//includes 사용
+const sol2 = (my_string, target) => my_string.includes(target) ? 1 :0;
+
+
+console.log(solution("banana","wxyz"));
+console.log(sol2("banana","ana"));
