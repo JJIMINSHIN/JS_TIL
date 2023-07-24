df3474e9f18aeb726c33ad1a203c166e1be17b7d
unknown <scalla1031@gmail.com>
2023-07-24 20:49:14 +0900

    CREATE : 수 조작하기 1

--- programmers/0단계/수 조작하기 1.js
+++ programmers/0단계/수 조작하기 1.js
@@ -1,1 +1,15 @@
 // 수 조작하기 1
+
+function solution(n, control) {
+    let str = control.split('');
+    for(let x  of str){
+        if(x=="w") n +=1;
+        else if(x == "s") n -= 1;
+        else if(x == "d") n += 10;
+        else if(x == "a") n -= 10;
+    }
+    return n;
+}
+
+let str = "wsdawsdassw"	
+console.log(solution(0, str));
