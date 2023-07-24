134a18e17bd41e4ab9f7ae903ea6e64ca589701e
unknown <scalla1031@gmail.com>
2023-07-24 20:40:16 +0900

    CREATE : 조건에 맞게 수열 변환하기 3

--- programmers/0단계/조건에 맞게 수열 변환하기 3.js
+++ programmers/0단계/조건에 맞게 수열 변환하기 3.js
@@ -1,1 +1,8 @@
 // 조건에 맞게 수열 변환하기 3
+
+function solution(arr, k) {
+    return k % 2 == 0 ? arr.map((v) => v+k) : arr.map((v) => v*k);
+}
+
+let arr = [1, 2, 3, 100, 99, 98];
+console.log(solution(arr, 2));
