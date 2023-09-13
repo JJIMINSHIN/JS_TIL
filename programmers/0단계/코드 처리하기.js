5cae84e6935e7bd72e7f5c3857e89bb8fd0ff0eb
JJIMINSHIN <scalla1031@gmail.com>
2023-09-13 17:53:16 +0900

    CREATE: 코드 처리하기

---
+++ programmers/0단계/코드 처리하기.js
@@ -0,0 +1,10 @@
+//코드 처리하기
+    
+function solution(my_string, is_suffix) {
+    return my_string.endsWith(is_suffix) ? 1 : 0;
+}
+
+let my_string = "banana";
+let is_suffix = "ana";
+console.log(solution(10))
+ 
