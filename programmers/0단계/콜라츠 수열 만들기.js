454fe60cf5cac834ad7855b65f287cb739ed6f17
JJIMINSHIN <scalla1031@gmail.com>
2023-12-03 16:22:06 +0900

    CREATE : 콜라츠 수열 만들기

---
+++ programmers/0단계/콜라츠 수열 만들기.js
@@ -0,0 +1,21 @@
+//콜라츠 수열 만들기
+
+
+function solution(n) {
+    var answer = [];
+   
+    while(n>0){
+        answer.push(n);
+
+        if(n==1) break;
+
+        if(n%2==0){
+            n= n/2;
+        }else if(n %2 !=0){
+            n = 3*n+1;
+        }
+    }
+    return answer;
+}
+
+console.log(solution(10));
